2d6bb94abe792ea05d5b7ab0fd12e65da6c55edc
Trudy Jones <tjones374@gmail.com>
2019-12-15 21:03:24 -0700

    Animate navbar w/css and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-function myFunction() {
-	var x = document.getElementById('myTopnav');
-	if (x.className === 'topnav') {
-		x.className += ' responsive';
-	} else {
-		x.className = 'topnav';
-	}
-}
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	//Toggle Nav
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+		//Animate Links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease froward ${index / 7 +
+					1}s`;
+			}
+		});
+		//Burger Animation
+		burger.classList.toggle('toggle');
+	});
+};
+
+navSlide();
