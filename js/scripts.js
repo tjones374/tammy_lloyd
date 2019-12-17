ae91348737dd95ebfbfd759e8c4f84844b8d10e4
Trudy Jones <tjones374@gmail.com>
2019-12-16 17:08:12 -0700

    Corrected divs in html/css and commented out js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+// 	const burger = document.querySelector('.burger');
+// 	const nav = document.querySelector('.nav-links');
+// 	const navLinks = document.querySelectorAll('.nav-links li');
 
-	//Toggle Nav
-	burger.addEventListener('click', () => {
-		nav.classList.toggle('nav-active');
-		//Animate Links
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease froward ${index / 7 +
-					1}s`;
-			}
-		});
-		//Burger Animation
-		burger.classList.toggle('toggle');
-	});
-};
+// 	//Toggle Nav
+// 	burger.addEventListener('click', () => {
+// 		nav.classList.toggle('nav-active');
+// 		//Animate Links
+// 		navLinks.forEach((link, index) => {
+// 			if (link.style.animation) {
+// 				link.style.animation = '';
+// 			} else {
+// 				link.style.animation = `navLinkFade 0.5s ease froward ${index / 7 +
+// 					1}s`;
+// 			}
+// 		});
+// 		//Burger Animation
+// 		burger.classList.toggle('toggle');
+// 	});
+// };
 
-navSlide();
+// navSlide();
